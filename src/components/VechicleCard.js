a8335f6dd0826a4008cffa64415102c552f663bd
ankitmhn <ankit.m@hurestic.com>
2020-05-21 23:27:13 +0530

    Highligting selected card

--- src/components/VechicleCard.js
+++ src/components/VechicleCard.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 //functional components do not accepts ref so wrapped in React.forwardRef
 //React.forwardRef accepts only two params
 const VehicleCard = React.forwardRef((props, ref) => {
   const { id, make, year, model, engineCapacity } = props.vehicle;
+  const selectedVehicleId = useSelector(
+    (state) => state.leftPane.selectedVehicleId
+  );
+  const isSelected = selectedVehicleId === id;
   return (
     <div
-      className="shadow rounded-lg m-4 p-2 h-50 bg-green-200 cursor-pointer text-gray-700"
+      className={`shadow rounded-lg m-4 p-2 h-50 cursor-pointer text-gray-700 ${
+        isSelected ? "bg-green-500" : "bg-green-200"
+      }`}
       onClick={() => props.selectCallback(id)}
       ref={ref || null}
     >
